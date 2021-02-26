291382c756d50e870548cf4e6f5b1e88ba360f1a
Giulio <giulio.tavoni@gmail.com>
2021-02-26 15:47:41 +0100

    Definizione layout

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- Link CSS -->
+  <link rel="stylesheet" href="css/style.css">
+  <!-- CDN Fontawesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+  <!-- Google Font -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+
+  <title>PHP Google FAQ</title>
+</head>
+<body>
+
+  <header>
+    <div class="left">
+      <img src="img/logo-google.png" alt="Logo Google">
+      <h2><a href="#">Privacy e termini</a></h2>
+    </div>
+    <div class="right">
+      <i class="fas fa-th"></i>
+      <a href="#" class="btn full-blue">Accedi</a>
+    </div>
+  </header>
+
+  <main>
+    <div class="top">
+      <ul>
+        <li><a href="#">Introduzione</a></li>
+        <li><a href="#">Norme sulla privacy</a></li>
+        <li><a href="#">Termini di servizio</a></li>
+        <li><a href="#">Tecnologie</a></li>
+        <li><a href="#" class="active">Domande frequenti</a></li>
+      </ul>
+    </div>
+
+
+  </main>
+
+</body>
+</html>
